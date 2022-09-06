c95a5fcf0495ec89109f80dbac874d30cafc16d7
seron <seroncj@gmail.com>
2022-09-06 09:55:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(userName, userEmail){
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+    .then(res => res.json())
+    .then(json => {document.querySelector('body').innerHTML = `<p>${json.id}</p>`})
+    .catch(error => {
+        alert("Bad things! Ragnarők!")
+        document.querySelector('body').innerHTML = `<p>${error.message}</p>`
+    })
+    }
